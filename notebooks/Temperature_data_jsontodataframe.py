c6fbcd419dadb0717dfd1b9b95546d60f537e465
leandroriverogonzalez <leandroriverogonzalez@gmail.com>
2022-09-02 11:51:07 -0300

    Agrego datos de temperatura en formato json y tambien el archivo python para pasar de json a pandas dataframe

---
+++ notebooks/Temperature_data_jsontodataframe.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import json
+import os
+
+# Cambiamos directorio si hace falta
+# os.chdir("/home/leandro/Documents/Maestria/SeriesdeTiempo/time-series-tp/notebooks")
+
+pathtemp_json = '../datasets/POWER_Point_Daily_20050101_20220831_034d8115S_058d2813W_LST.json'
+
+f = open(pathtemp_json)
+temp_json = json.load(f)
+df_temp = pd.DataFrame(list(zip(list(tj.keys()), list(tj.values()))), columns =['Fecha', 'T_mean'])
+df_temp['Fecha'] = pd.to_datetime(df_temp['Fecha'], format='%Y%m%d')
+df_temp = df_temp.sort_values('Fecha')
