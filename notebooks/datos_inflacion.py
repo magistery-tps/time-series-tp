e9bcbe4216c3a432b825763d8ce2d578e95ed6cb
leandroriverogonzalez <leandroriverogonzalez@gmail.com>
2022-09-02 19:32:35 -0300

    Agrego en el codigo de inflación que calcule la inflación acumulada, también agrego el csv que sale de ese calculo

--- notebooks/datos_inflacion.py
+++ notebooks/datos_inflacion.py
@@ -8,6 +8,7 @@ from calendar import monthrange
 plt.rcParams.update({'font.size': 22})
 path_consumo = '../datasets/inflacion.csv'
 
+# Agrego algunas columnas y calculo la inflacion diaria, considerando la inflación mensual y la cantidad de días del mes
 datos_inflacion = pd.read_csv(path_consumo)
 datos_inflacion['Año'] = datos_inflacion['Año'].astype('Int64')
 datos_inflacion['Mes'] = datos_inflacion['Mes'].astype('Int64')
@@ -17,8 +18,8 @@ datos_inflacion['n_daymonth'] = datos_inflacion['fecha'].dt.days_in_month.astype
 datos_inflacion['inflacion_mas_1'] = (1 + 0.01*datos_inflacion.Inflacion).astype(float)
 datos_inflacion = datos_inflacion.assign(inflacion_diaria=lambda x: x['inflacion_mas_1']**(1/x['n_daymonth']))
 
+# Ploteo los datos de inflación por mes
 plt.plot(datos_inflacion.fecha, datos_inflacion.inflacion_diaria)
-plt.legend()
 plt.ylabel('Precio')
 plt.xlabel('Fecha [Año]')
 plt.show()
@@ -31,7 +32,6 @@ ffecha = datetime(2022,1,1)
 cols_interes = ['Inflacion', 'Unnamed: 4','Fuente', 'inflacion_diaria']
 
 # Trabajo con mi consumo eléctrico
-fechas_inf = datos_ce.fecha.values
 fechas_todas = pd.date_range(cfecha, ffecha-timedelta(days=1), freq='d').values
 fechas_df = pd.DataFrame(list(set(fechas_todas) - set(fechas_inf)), columns=['fecha'])
 datos_inflacion = pd.concat([datos_inflacion, fechas_df], ignore_index=True)
@@ -39,7 +39,8 @@ datos_inflacion = datos_inflacion.sort_values('fecha', ascending=True).reset_ind
 
 # Replico el valor de precio de temperatura del 1/12/2021 en el 31/12/2021
 
-# Teniendo los datos de consumo ordenado por fecha descendente. Replico el dato anterior, siempre y cuando no haya un dato nuevo
+# Teniendo los datos de inflacion ordenado por fecha ascendente.
+# Replico el dato anterior hasta que haya uno nuevo
 for i in range(datos_inflacion.shape[0]):
     df_trato = datos_inflacion.loc[i, cols_interes].copy()
     if not(math.isnan(df_trato.inflacion_diaria)):
@@ -47,9 +48,16 @@ for i in range(datos_inflacion.shape[0]):
     else:
         datos_inflacion.loc[i, cols_interes] = df_sirve
 
+# Me quedo con las columnas de interés
+datos_inflacion = datos_inflacion[['fecha', 'inflacion_diaria', 'Fuente']]
+
+# Calculo la inflación acumulada día a día
+inf_acumulada = [1]
+for i, value in enumerate(datos_inflacion.inflacion_diaria.values[1:]):
+    inf_acumulada.append(inf_acumulada[i] * value)
+datos_inflacion['inflacion_acumulada'] = inf_acumulada
 
 plt.plot(datos_inflacion.fecha, datos_inflacion.inflacion_diaria)
-plt.legend()
 plt.ylabel('Inflacion diaria')
 plt.xlabel('Fecha [Año]')
 plt.show()
